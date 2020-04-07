5c4d74b6c63bd341269b2a4aff20b784aa35f2eb
AntonBergaker <noten.b@hotmail.com>
2020-04-07 09:04:05 +0200

    Turned segment list into pointers

--- src/navigation.cpp
+++ src/navigation.cpp
@@ -1,15 +1,17 @@
 #include "navigation.h"
+#include <QtLocation/QGeoRouteReply>
+#include <QtLocation/QGeoRouteSegment>
 
 Navigation::Navigation(QGeoRoute geoRoute, QObject *parent) : QObject(parent)
 {
     source_ = geoRoute;
 
-    segments_ = QList<QGeoRouteSegment>();
+    segments_ = QList<QGeoRouteSegment*>();
 
     QGeoRouteSegment seg = geoRoute.firstRouteSegment();
 
     while (seg.isValid()) {
-        segments_.append(seg);
+        segments_.append(&seg);
 
         if (seg.isLegLastSegment()) {
             break;
--- src/navigation.h
+++ src/navigation.h
@@ -2,25 +2,24 @@
 #define NAVIGATION_H
 
 #include <QObject>
-#include <QtLocation/QGeoRouteReply>
+#include <QtLocation/QGeoRoute>
 
 class Navigation : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QGeoRouteReply* source READ source CONSTANT)
-    Q_PROPERTY(QList<QGeoRouteSegment>* segments READ segments CONSTANT)
+    Q_PROPERTY(QGeoRoute source READ source CONSTANT)
+    Q_PROPERTY(QList<QGeoRouteSegment*> segments READ segments CONSTANT)
 public:
     explicit Navigation(QGeoRoute geoRoute, QObject *parent = nullptr);
-    ~Navigation();
-    QGeoRouteReply source() {return source_;}
-    QList<QGeoRouteSegment> segments() {return segments_;}
+    QGeoRoute source() {return source_;}
+    QList<QGeoRouteSegment*> segments() {return segments_;}
 signals:
 
 public slots:
 
 private:
-    QGeoRouteReply source_;
-    QList<QGeoRouteSegment> segments_;
+    QGeoRoute source_;
+    QList<QGeoRouteSegment*> segments_;
 };
 
 #endif // NAVIGATION_H
