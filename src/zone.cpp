ad77c665cf8b41b148399e6e1f4389e633e10542
Arvid <arvid.sandin@gmail.com>
2020-04-21 11:46:08 +0200

    Zone fungerar förhoppningsvis

--- src/zone.cpp
+++ src/zone.cpp
@@ -1,17 +1,17 @@
 #include "zone.h"
 
-Zone::Zone(Qlist<QGeoCoordinate> coordinates, QString name, QObject *parent) : QObject(parent)
+Zone::Zone(QList<QGeoCoordinate> coordinates, QString name, QObject *parent) : QObject(parent)
 {
-    Qlist<QGeoCoordinate> coordinates_ = coordinates;
+    QList<QGeoCoordinate> coordinates_ = coordinates;
     QString name_ = name;
 
     QGeoCoordinate averagePoint_;
-    for (int var = 0; var < coordinates.length; ++var) {
-        averagePoint_.setLongitude(averagePoint_.longitude() + coordinates[i].longitude);
-        averagePoint_.setLatitude(averagePoint_.latitude() + coordinates[i].latitude);
+    for (int i = 0; i < coordinates.length(); ++i) {
+        averagePoint_.setLongitude(averagePoint_.longitude() + coordinates[i].longitude());
+        averagePoint_.setLatitude(averagePoint_.latitude() + coordinates[i].latitude());
     }
-    averagePoint_.setLongitude(averagePoint_.longitude() / coordinates.length);
-    averagePoint_.setLatitude(averagePoint_.latitude() / coordinates.length);
+    averagePoint_.setLongitude(averagePoint_.longitude() / coordinates.length());
+    averagePoint_.setLatitude(averagePoint_.latitude() / coordinates.length());
 }
 Zone::~Zone()
 {
--- src/zone.h
+++ src/zone.h
@@ -11,9 +11,12 @@ class Zone : public QObject
     Q_PROPERTY(QList<QGeoCoordinate> coordinates READ coordinates CONSTANT)
     Q_PROPERTY(QString name READ name CONSTANT)
 public:
+    explicit Zone(QObject *parent = nullptr) {}
+    explicit Zone(const Zone &other) {}
     explicit Zone(QList<QGeoCoordinate> coordinates, QString name, QObject *parent = nullptr);
     ~Zone();
     QList<QGeoCoordinate> coordinates() {return coordinates_;}
+    QGeoCoordinate averagePoint() {return averagePoint_;}
     QString name() {return name_;}
 signals:
 
@@ -22,6 +25,7 @@ public slots:
 private:
     QList<QGeoCoordinate> coordinates_;
     QString name_;
+    QGeoCoordinate averagePoint_;
 };
 
 #endif // ZONE_H
